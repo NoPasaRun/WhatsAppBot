cb60ee01834a2b74e53f3f463ca60e0b6dab4a80
NoPasaRun <bogdanbelenesku@gmail.com>
2022-06-26 15:32:34 +0300

    Changed postgresql url

--- application/database.py
+++ application/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 
-database_url = "postgres+asyncpg://codetkbctroumj:431aa6a6c15d78ccec1c371dda3c4865c71af6424061542bd0951fd7bd174b03@" \
+database_url = "postgresql+asyncpg://codetkbctroumj:431aa6a6c15d78ccec1c371dda3c4865c71af6424061542bd0951fd7bd174b03@" \
                "ec2-54-228-32-29.eu-west-1.compute.amazonaws.com:5432/dd4j2irjr1k381"
 engine = create_async_engine(database_url, echo=True)
 
