b9ed5dd7bebf36fea8e47cd8ea50f2f8a3439275
NoPasaRun <bogdanbelenesku@gmail.com>
2022-06-26 21:44:04 +0300

    total finished function wich sends back messages

--- application/app.py
+++ application/app.py
@@ -46,6 +46,6 @@ async def print_recived_messages(request: Request) -> tuple:
     data = {"message": data["messageData"]["textMessageData"]["textMessage"], "chatId": data["senderData"]["chatId"]}
     url = f"https://api.green-api.com/waInstance{config['IdInstance']}/SendMessage/{config['apiTokenInstance']}"
     async with aiohttp.ClientSession() as http_session:
-        async with http_session.post(url=url, data=data) as response:
+        async with http_session.post(url=url, data=json.dumps(data)) as response:
             content = await response.text()
     return content, response.status
